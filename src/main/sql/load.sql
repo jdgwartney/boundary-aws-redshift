3c01c42d578272b44c9c8224bd124017c170a310
jdgwartney <davidg@boundary.com>
2014-11-10 13:44:37 -0800

    Remove spaces in paths and key place holders

--- src/main/sql/load.sql
+++ src/main/sql/load.sql
@@ -4,8 +4,8 @@ CREDENTIALS 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-
 copy venue from 's3://<region-specific-bucket-name>/tickit/venue_pipe.txt'
 CREDENTIALS 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>' delimiter '|';
 
-copy category from 's3://<region-specific-bucket-name>/tickit/cat egory_pipe.txt'
-CREDENTIALS 'aws_access_key_id=<Your-Access-Key- ID>;aws_secret_access_key=<Your-Secret-Access-Key>' delimiter '|';
+copy category from 's3://<region-specific-bucket-name>/tickit/category_pipe.txt'
+CREDENTIALS 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>' delimiter '|';
 
 copy date from 's3://<region-specific-bucket-name>/tickit/date2008_pipe.txt'
 CREDENTIALS 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>' delimiter '|';
@@ -13,8 +13,8 @@ CREDENTIALS 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-
 copy event from 's3://<region-specific-bucket-name>/tickit/allevents_pipe.txt'
 CREDENTIALS 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>' delimiter '|' timeformat 'YYYY-MM-DD HH:MI:SS';
 
-copy listing from 's3://<region-specific-bucket-name>/tickit/list ings_pipe.txt'
-CREDENTIALS 'aws_access_key_id=<Your-Access-Key- ID>;aws_secret_access_key=<Your-Secret-Access-Key>' delimiter '|';
+copy listing from 's3://<region-specific-bucket-name>/tickit/listings_pipe.txt'
+CREDENTIALS 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>' delimiter '|';
 
 copy sales from 's3://<region-specific-bucket-name>/tickit/sales_tab.txt'
 CREDENTIALS 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>' delimiter '\t' timeformat 'MM/DD/YYYY HH:MI:SS';
